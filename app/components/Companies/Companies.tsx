15e0e74a427c5502255235eb411e960106bce6f5
YajvendraSaini <yajvendra98@gmail.com>
2024-12-04 16:50:44 +0530

    added logo

--- app/components/Companies/Companies.tsx
+++ app/components/Companies/Companies.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 
 // IMAGES DATA FOR CAROUSEL
@@ -10,93 +10,111 @@ interface Data {
 
 const data: Data[] = [
     {
-        imgSrc: "/assets/carousel/google.svg"
+        imgSrc: "/assets/carousel/1.png"
     },
     {
-        imgSrc: "/assets/carousel/garnier.png"
+        imgSrc: "/assets/carousel/2.png"
     },
     {
-        imgSrc: "/assets/carousel/slack.png"
+        imgSrc: "/assets/carousel/3.png"
     },
     {
-        imgSrc: "/assets/carousel/udemy.png"
+        imgSrc: "/assets/carousel/4.png"
     },
     {
-        imgSrc: "/assets/carousel/google.svg"
+        imgSrc: "/assets/carousel/5.png"
     },
     {
-        imgSrc: "/assets/carousel/garnier.png"
+        imgSrc: "/assets/carousel/6.png"
     },
     {
-        imgSrc: "/assets/carousel/slack.png"
+        imgSrc: "/assets/carousel/7.png"
     },
     {
-        imgSrc: "/assets/carousel/udemy.png"
+        imgSrc: "/assets/carousel/8.png"
+    },
+    {
+        imgSrc: "/assets/carousel/9.png"
+    },
+    {
+        imgSrc: "/assets/carousel/10.png"
+    },
+    {
+        imgSrc: "/assets/carousel/11.png"
+    },
+    {
+        imgSrc: "/assets/carousel/12.png"
+    },
+    {
+        imgSrc: "/assets/carousel/13.png"
+    },
+    {
+        imgSrc: "/assets/carousel/14.png"
+    },
+    {
+        imgSrc: "/assets/carousel/15.png"
+    },
+    {
+        imgSrc: "/assets/carousel/16.png"
     }
+
+    
 ]
 
-
-// CAROUSEL SETTINGS
-export default class MultipleItems extends Component {
-    render() {
-        const settings = {
-            dots: false,
-            infinite: true,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            speed: 2000,
-            autoplaySpeed: 2000,
-            cssEase: "linear",
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 700,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 500,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
+const Companies = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed: 3000,
+        autoplaySpeed: 0,
+        cssEase: "linear",
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
                 }
-            ]
-        };
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    };
 
-        return (
-
-            <div className='text-center'>
-                <div className="mx-auto max-w-4xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                    <div className="py-14">
-                        <Slider {...settings}>
-                            {data.map((item, i) =>
-                                <div key={i}>
-                                    <Image src={item.imgSrc} alt={item.imgSrc} width={150} height={150} />
-                                </div>
-                            )}
-                        </Slider>
+    return (
+        <div className='mx-auto max-w-7xl sm:py-4 lg:px-8'>
+            <Slider {...settings}>
+                {data.map((item, i) => (
+                    <div key={i} className="flex items-center justify-center h-full">
+                        <Image 
+                            src={item.imgSrc} 
+                            alt="carousel-img" 
+                            width={180} 
+                            height={120} 
+                            className="object-contain"
+                        />
                     </div>
-                    <hr />
-                </div>
-            </div>
-
-        )
-    }
+                ))}
+            </Slider>
+        </div>
+    )
 }
+
+export default Companies;
